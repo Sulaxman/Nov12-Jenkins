fed464425647cae5c1a5073b4c9023da33f91386
Kaja <sulaxman.a.kaja@accenture.com>
2018-11-12 14:51:14 +0530

    Maven project commit for my first Jenkins Job

---
+++ src/main/java/MyFirstApp/JenkinsSample/App.java
@@ -0,0 +1,13 @@
+package MyFirstApp.JenkinsSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
